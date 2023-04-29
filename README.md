e5ea585ea5ce23d8982b0245218af576ac8cbe5c
Michelangelo <you@example.com>
2023-04-29 11:37:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# chatgpt_selenium_automation
+
+ChatGPT Automation is a Python project that aims to automate interactions with OpenAI's ChatGPT using Selenium WebDriver. Currently, it requires human interaction for log-in and human verification. It handles launching Chrome, connecting to ChatGPT, sending prompts, and retrieving responses. This tool can be useful for experimenting with ChatGPT or building similar web automation tools.
+
+
+## Prerequisites
+
+1. Make sure you have installed the required libraries, as specified in the `requirements.txt` file.
+2. Download the appropriate version of `chromedriver.exe` and save it to a known location on your system.
+
+
+## Example Usage
+
+ ```python
+from handler.chatgpt_selenium_automation import ChatGPTAutomation
+
+# Define the path where the chrome driver is installed on your computer
+chrome_driver_path = r"C:\Users\user\Desktop\chromedriver.exe"
+
+# the sintax r'"..."' is required because the space in "Program Files" in the chrome path
+chrome_path = r'"C:\Program Files\Google\Chrome\Application\chrome.exe"'
+
+# Create an instance
+chatgpt = ChatGPTAutomation(chrome_path, chrome_driver_path)
+
+# Define a prompt and send it to chatgpt
+prompt = "What are the benefits of exercise?"
+chatgpt.send_prompt_to_chatgpt(prompt)
+
+# Retrieve the last response from ChatGPT
+response = chatgpt.return_last_response()
+print(response)
+
+# Save the conversation to a text file
+file_name = "conversation.txt"
+chatgpt.save_conversation(file_name)
+
+# Close the browser and terminate the WebDriver session
+chatgpt.quit()
+   ```
+   
+   
+## Note 
+
+After instantiating the ChatGPTAutomation class, chrome will open up to chatgpt page, it will require you to manually complete the register/ log-in / Human-verification. After that, you must tell the program to continue, in the console type 'y'. After Those steps, the program will be able to continue autonomously.
+
+## Note on Changing Tabs or Conversations
+
+Please be aware that changing tabs or switching to another conversation while the script is running might cause errors or lead to the methods being applied to unintended chats. For optimal results and to avoid unintended consequences, it is recommended to avoid to manually interact with the browser (after the log-in/human verification) while the automation script is running.
+
+   
+   
+## Note on Errors and Warnings
+
+While running the script, you may see some error messages or warnings in the console, such as:
+- DevTools listening on ws://...
+- INFO: Created TensorFlow Lite XNNPACK delegate for CPU.
+- ERROR: Couldn't read tbsCertificate as SEQUENCE
+- ERROR: Failed parsing Certificate
+   
+
+These messages are related to the underlying libraries or the browser, and you can safely ignore them if the script works as expected. If you encounter any issues with the script, please ensure that you have installed the correct dependencies and are using the correct ChromeDriver version compatible with your Chrome browser.
+
+   
+   
+
---
+++ handler/chatgpt_selenium_automation.py
@@ -0,0 +1,149 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+import time
+import socket
+import threading
+import os
+
+
+
+class ChatGPTAutomation:
+
+    def __init__(self, chrome_path, chrome_driver_path):
+        """
+        This constructor automates the following steps:
+        1. Open a Chrome browser with remote debugging enabled at a specified URL.
+        2. Prompt the user to complete the log-in/registration/human verification, if required.
+        3. Connect a Selenium WebDriver to the browser instance after human verification is completed.
+
+        :param chrome_path: file path to chrome.exe (ex. C:\\Users\\User\\...\\chromedriver.exe)
+        :param chrome_driver_path: file path to chrome.exe (ex. C:\\Users\\User\\...\\chromedriver.exe)
+        """
+
+        self.chrome_path = chrome_path
+        self.chrome_driver_path = chrome_driver_path
+
+        url = r"https://chat.openai.com"
+        free_port = self.find_available_port()
+        self.launch_chrome_with_remote_debugging(free_port, url)
+        self.wait_for_human_verification()
+        self.driver = self.setup_webdriver(free_port)
+
+
+
+    def find_available_port(self):
+        """ This function finds and returns an available port number on the local machine by creating a temporary
+            socket, binding it to an ephemeral port, and then closing the socket. """
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('', 0))
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            return s.getsockname()[1]
+
+
+
+    def launch_chrome_with_remote_debugging(self, port, url):
+        """ Launches a new Chrome instance with remote debugging enabled on the specified port and navigates to the
+            provided url """
+
+        def open_chrome():
+            chrome_cmd = f"{self.chrome_path} --remote-debugging-port={port} --user-data-dir=remote-profile {url}"
+            os.system(chrome_cmd)
+
+        chrome_thread = threading.Thread(target=open_chrome)
+        chrome_thread.start()
+
+
+
+    def setup_webdriver(self, port):
+        """  Initializes a Selenium WebDriver instance, connected to an existing Chrome browser
+             with remote debugging enabled on the specified port"""
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("debuggerAddress", f"127.0.0.1:{port}")
+        driver = webdriver.Chrome(executable_path=self.chrome_driver_path, options=chrome_options)
+        return driver
+
+
+
+    def send_prompt_to_chatgpt(self, prompt):
+        """ Sends a message to ChatGPT and waits for 20 seconds for the response """
+
+        input_box = self.driver.find_element(by=By.XPATH, value='//textarea[contains(@placeholder, "Send a message")]')
+        self.driver.execute_script(f"arguments[0].value = '{prompt}';", input_box)
+        input_box.send_keys(Keys.RETURN)
+        time.sleep(20)
+
+
+
+    def return_chatgpt_conversation(self):
+        """
+        :return: returns a list of items, even items are the submitted questions (prompts) and odd items are chatgpt response
+        """
+
+        return self.driver.find_elements(by=By.CSS_SELECTOR, value='div.text-base')
+
+
+
+    def save_conversation(self, file_name):
+        """
+        It saves the full chatgpt conversation of the tab open in chrome into a text file, with the following format:
+            prompt: ...
+            response: ...
+            delimiter
+            prompt: ...
+            response: ...
+
+        :param file_name: name of the file where you want to save
+        """
+
+        directory_name = "conversations"
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+
+        delimiter = "|^_^|"
+        chatgpt_conversation = self.return_chatgpt_conversation()
+        with open(os.path.join(directory_name, file_name), "a") as file:
+            for i in range(0, len(chatgpt_conversation), 2):
+                file.write(
+                    f"prompt: {chatgpt_conversation[i].text}\nresponse: {chatgpt_conversation[i + 1].text}\n\n{delimiter}\n\n")
+
+
+
+    def return_last_response(self):
+        """ :return: the text of the last chatgpt response """
+
+        response_elements = self.driver.find_elements(by=By.CSS_SELECTOR, value='div.text-base')
+        return response_elements[-1].text
+
+
+
+    def wait_for_human_verification(self):
+        print("You need to manually complete the log-in or the human verification if required.")
+
+        while True:
+            user_input = input(
+                "Enter 'y' if you have completed the log-in or the human verification, or 'n' to check again: ").lower()
+
+            if user_input == 'y':
+                print("Continuing with the automation process...")
+                break
+            elif user_input == 'n':
+                print("Waiting for you to complete the human verification...")
+                time.sleep(5)  # You can adjust the waiting time as needed
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
+
+
+
+    def quit(self):
+        """ Closes the browser and terminates the WebDriver session."""
+        print("Closing the browser...")
+        self.driver.close()
+        self.driver.quit()
+
+
+
+
