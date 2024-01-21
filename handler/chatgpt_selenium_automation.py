51145b05e59118d68732430ad05f8905e9c88cad
Matteo Rizzo <matteo.rizzo.phd@gmail.com>
2024-01-21 17:47:17 +0100

    Update chatgpt_selenium_automation.py (#4)
    outdated arguments

--- handler/chatgpt_selenium_automation.py
+++ handler/chatgpt_selenium_automation.py
@@ -1,12 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
-import time
+import os
 import socket
 import threading
-import os
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class ChatGPTAutomation:
@@ -31,9 +30,8 @@ class ChatGPTAutomation:
         self.wait_for_human_verification()
         self.driver = self.setup_webdriver(free_port)
 
-
-
-    def find_available_port(self):
+    @staticmethod
+    def find_available_port():
         """ This function finds and returns an available port number on the local machine by creating a temporary
             socket, binding it to an ephemeral port, and then closing the socket. """
 
@@ -41,8 +39,6 @@ class ChatGPTAutomation:
             s.bind(('', 0))
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             return s.getsockname()[1]
-
-
 
     def launch_chrome_with_remote_debugging(self, port, url):
         """ Launches a new Chrome instance with remote debugging enabled on the specified port and navigates to the
@@ -55,29 +51,24 @@ class ChatGPTAutomation:
         chrome_thread = threading.Thread(target=open_chrome)
         chrome_thread.start()
 
-
-
     def setup_webdriver(self, port):
         """  Initializes a Selenium WebDriver instance, connected to an existing Chrome browser
              with remote debugging enabled on the specified port"""
 
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = self.chrome_driver_path
         chrome_options.add_experimental_option("debuggerAddress", f"127.0.0.1:{port}")
-        driver = webdriver.Chrome(executable_path=self.chrome_driver_path, options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
         return driver
-
-
 
     def send_prompt_to_chatgpt(self, prompt):
         """ Sends a message to ChatGPT and waits for 20 seconds for the response """
 
-        input_box = self.driver.find_element(by=By.XPATH, value='//textarea[contains(@placeholder, "Send a message")]')
+        input_box = self.driver.find_element(by=By.XPATH, value='//textarea[contains(@id, "prompt-textarea")]')
         self.driver.execute_script(f"arguments[0].value = '{prompt}';", input_box)
         input_box.send_keys(Keys.RETURN)
         input_box.submit()
         time.sleep(20)
-
-
 
     def return_chatgpt_conversation(self):
         """
@@ -85,8 +76,6 @@ class ChatGPTAutomation:
         """
 
         return self.driver.find_elements(by=By.CSS_SELECTOR, value='div.text-base')
-
-
 
     def save_conversation(self, file_name):
         """
@@ -111,17 +100,14 @@ class ChatGPTAutomation:
                 file.write(
                     f"prompt: {chatgpt_conversation[i].text}\nresponse: {chatgpt_conversation[i + 1].text}\n\n{delimiter}\n\n")
 
-
-
     def return_last_response(self):
         """ :return: the text of the last chatgpt response """
 
         response_elements = self.driver.find_elements(by=By.CSS_SELECTOR, value='div.text-base')
         return response_elements[-1].text
 
-
-
-    def wait_for_human_verification(self):
+    @staticmethod
+    def wait_for_human_verification():
         print("You need to manually complete the log-in or the human verification if required.")
 
         while True:
@@ -137,14 +123,8 @@ class ChatGPTAutomation:
             else:
                 print("Invalid input. Please enter 'y' or 'n'.")
 
-
-
     def quit(self):
         """ Closes the browser and terminates the WebDriver session."""
         print("Closing the browser...")
         self.driver.close()
         self.driver.quit()
-
-
-
-
